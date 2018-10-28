7a4769374201b497de4977c5cc29daad249bd978
Emmanuel M <emmanuelbendavid@gmail.com>
2018-10-28 15:18:01 -0300

    Added file names as comments to compiler

--- fusion.sh
+++ fusion.sh
@@ -1,5 +1,10 @@
-cat ./src/data.s		>	./build/merged-file.s
+echo "# FILE data.s"		>	./build/merged-file.s
+cat ./src/data.s		>>	./build/merged-file.s
+echo "# FILE $1.s"		>>	./build/merged-file.s
 cat "./src/$1.s" 		>>	./build/merged-file.s
+echo "# FILE shuntingyard.s"	>>	./build/merged-file.s
 cat ./src/shuntingyard.s	>>	./build/merged-file.s
+echo "# FILE token.s"		>>	./build/merged-file.s
 cat ./src/token.s		>>	./build/merged-file.s
+echo "# FILE deque.s"		>>	./build/merged-file.s
 cat ./src/deque.s 		>>	./build/merged-file.s
